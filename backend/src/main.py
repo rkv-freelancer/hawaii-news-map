6109025f0d3e35121ce397a49fb47efbd5cdcbc4
rkv-freelancer <143656295+rkv-freelancer@users.noreply.github.com>
2026-01-10 18:05:46 -1000

    imported `requests` and `feedparser`

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,13 @@
+# Feedfinder.py
+
 # https://docs.astral.sh/uv/
 # https://drivendata.github.io/cookiecutter-data-science/
+# https://dev.to/jps27cse/understanding-backend-folder-structure-a-beginners-guide-8a7
 
 # Obtain RSS Feeds 
 from pydantic import BaseModel, ConfigDict
 
 # Read a Parser
+import requests
+import feedparser
+import urllib.parse
