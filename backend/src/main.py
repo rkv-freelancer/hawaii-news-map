202a4611708ce46e128b7baa093d0240a7d59425
rkv-freelancer <143656295+rkv-freelancer@users.noreply.github.com>
2026-01-11 17:28:50 -1000

    successfully parsed XML data from url sites

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,3 +1,4 @@
+# https://extensionpay.com/articles/browser-extensions-make-money
 # Feedfinder.py
 
 # https://docs.astral.sh/uv/
@@ -11,3 +12,62 @@ from pydantic import BaseModel, ConfigDict
 import requests
 import feedparser
 import urllib.parse
+
+"""
+item
+│
+├── title (string)
+├── link (URL string)
+├── dc:creator (string)
+├── pubDate (string)
+│
+├── category (array of strings)ow ow 
+│     ├── category
+│     ├── category
+│     └── … (0…N)
+│
+├── guid (string + isPermaLink attr)
+│
+├── description (string/HTML)
+│
+├── enclosure (optional)
+│     ├── url
+│     ├── type
+│     └── length
+│
+└── dcterms:modified (string – ISO 8601)
+
+"""
+
+# Print To Console.log
+def findFeed(url: str): 
+    f = feedparser.parse(url)
+    if len(f.entries) > 0:
+        print("-" * 20)
+        print(f"Found: {len(f.entries)}")
+        print("-" * 20, "\n")
+        
+        # Check if it is a `Paywall`
+        
+        # Obtain As Index
+        for entry in f.entries: 
+            print(f"Title: {entry.get("title", "No title available")}")
+            print(f"Link: {entry.get("link", 'No link available')}")
+            
+            # Use .get() method to safely access elements that might not exist in every feed
+            print(f"Description: {entry.get('description', 'No summary available')}") 
+            print("-" * 20)
+        
+if __name__ == "__main__": 
+    # Main URL 
+    # Add To Multiple Feeds
+    url_local_news: str = "https://www.khon2.com/top-stories/feed/"
+    findFeed(url = url_local_news)
+
+
+# Prenium Stories? 
+
+# Feed To LLM to Generate Geo Information for News Article
+# Attempt #1: No to Categories (Multiple  News), `<![CDATA[ ... ]]>`
+# Attempt #2: 
+
